6d252dcd3d5c4b861c7106181d548d56dfbb91dc
LuisAg1256 <laaguilar21@utpl.edu.ec>
2026-05-04 16:00:42 +0000

    Despliegue final con 50 registros de Loja

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import './style.css';
 import DataTable from 'datatables.net-dt';
 import 'datatables.net-dt/css/dataTables.dataTables.css';
 
-const API_URL = "https://raw.githubusercontent.com/PlataformasWeb-P-AA2026/api-demo/main/db.json"
+const API_URL = "./data/mis_datos.json"
 
 async function cargarDatos() {
   try {
